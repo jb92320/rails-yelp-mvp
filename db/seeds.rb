e47e9cb3f83b10db5f0dd6b4b52ccf28dbc08f4a
Jean-Baptiste Bernard <jb.bernard92@gmail.com>
2023-05-24 16:57:55 +0200

    exercice done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Creating restaurants..."
+sphere = { name: "Sphère", address: "18 Rue la Boétie, 75008 Paris", category: "french" }
+passione = { name: "Passioné", address: "17 Rue Bergère, 75009 Paris", category: "french" }
+alliance = { name: "Alliance", address: "5 Rue de Poissy, 75005 Paris", category: "french" }
+
+
+[sphere, passione,].each do |attributes|
+  review = Review.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished"
